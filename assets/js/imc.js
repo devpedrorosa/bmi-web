5daee1b6405295b4b9516f8cd845e3ff7e215216
Pedro Rosa <devpedrorosa@gmail.com>
2023-11-19 23:22:08 -0300

    Calculadora Implatada porem pega numeros infinitos

--- assets/js/imc.js
+++ assets/js/imc.js
@@ -1,19 +1,97 @@
-// assets/js/imc.js
+document.addEventListener('DOMContentLoaded', function () {
+  const pesoInput = document.getElementById('peso');
+  const alturaInput = document.getElementById('altura');
+  const resultadoDiv = document.getElementById('result');
 
-// Função para calcular o IMC
-function calcularIMC(event) {
-  event.preventDefault(); // Impede o recarregamento da página
+  const formatarPesoAltura = function (valor) {
+    // Remove caracteres não numéricos
+    const numeroFormatado = valor.replace(/[^0-9.]/g, '');
 
-  // Obter os valores de peso e altura do formulário
-  const peso = parseFloat(document.getElementById('peso').value);
-  const altura = parseFloat(document.getElementById('altura').value);
+    // Verifica se o valor é vazio ou não numérico
+    if (numeroFormatado === '' || isNaN(numeroFormatado)) {
+      return '';
+    }
 
-  // Calcular o IMC
-  const imc = peso / (altura * altura);
+    // Adiciona a vírgula para representar o ponto flutuante
+    const numeroComoFloat = parseFloat(numeroFormatado.replace(',', '.'));
 
-  // Exibir o resultado na tela
-  document.getElementById('result').innerHTML = `Seu IMC é: ${imc.toFixed(2)}`;
-}
+    return numeroComoFloat;
+  };
 
-// Adicionar evento de clique ao botão de calcular
-document.getElementById('btn').addEventListener('click', calcularIMC);
+  const adicionarZeros = function (valor, casasDecimais) {
+    const partes = valor.toString().split('.');
+    if (partes.length === 1) {
+      return valor.toFixed(casasDecimais);
+    } else {
+      const parteDecimal = partes[1];
+      if (parteDecimal.length < casasDecimais) {
+        return valor.toFixed(casasDecimais);
+      }
+      return valor;
+    }
+  };
+
+  pesoInput.addEventListener('input', function (event) {
+    const cursorPosition = event.target.selectionStart;
+    let peso = formatarPesoAltura(event.target.value);
+    peso = isNaN(peso) ? '' : peso.toString();
+    event.target.value = peso === '' ? '' : adicionarZeros(peso, 1);
+    event.target.setSelectionRange(cursorPosition, cursorPosition);
+  });
+
+  alturaInput.addEventListener('input', function (event) {
+    const cursorPosition = event.target.selectionStart;
+    let altura = formatarPesoAltura(event.target.value);
+    altura = isNaN(altura) ? '' : altura.toString();
+
+    // Adiciona automaticamente o ponto e zeros após o primeiro número
+    if (altura.length === 1) {
+      altura += '.';
+    }
+
+    event.target.value = altura === '' ? '' : adicionarZeros(altura, 2);
+    event.target.setSelectionRange(cursorPosition, cursorPosition);
+  });
+
+  const calcularIMC = function (peso, altura) {
+    if (isNaN(peso) || isNaN(altura) || altura === 0) {
+      return 'Altura inválida';
+    }
+
+    const imc = peso / (altura * altura);
+    return imc.toFixed(2);
+  };
+
+  const categorizarIMC = function (imc) {
+    if (imc === 'Altura inválida') {
+      return 'Altura inválida';
+    } else if (imc <= 18.5) {
+      return 'Abaixo do normal';
+    } else if (imc <= 24.9) {
+      return 'Normal';
+    } else if (imc <= 29.9) {
+      return 'Sobrepeso';
+    } else if (imc <= 35) {
+      return 'Obesidade grau 1';
+    } else if (imc <= 40) {
+      return 'Obesidade grau 2';
+    } else {
+      return 'Obesidade grau 3';
+    }
+  };
+
+  const exibirResultado = function (imc, categoria) {
+    resultadoDiv.innerHTML = `Seu IMC é ${imc}. Categoria: ${categoria}`;
+  };
+
+  const form = document.getElementById('box-form');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const peso = parseFloat(pesoInput.value.replace(',', '.'));
+    const altura = parseFloat(alturaInput.value.replace(',', '.'));
+
+    const imc = calcularIMC(peso, altura);
+    const categoria = categorizarIMC(imc);
+    exibirResultado(imc, categoria);
+  });
+});
