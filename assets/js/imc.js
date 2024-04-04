4324ceb839ce6451087a2d8d140566c91477376f
pedrorosa-dev <phrosa.c@gmail.com>
2024-04-04 14:17:50 -0300

    chore(imc.js): transformar a altura em metros

--- assets/js/imc.js
+++ assets/js/imc.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   const pesoInput = document.getElementById('peso');
   const alturaInput = document.getElementById('altura');
   const resultadoDiv = document.getElementById('result');
 
-  const formatarPesoAltura = function (valor) {
+  const formatarPesoAltura = function(valor) {
     // Remove caracteres não numéricos
     const numeroFormatado = valor.replace(/[^0-9.]/g, '');
 
@@ -12,48 +12,69 @@ document.addEventListener('DOMContentLoaded', function () {
       return '';
     }
 
-    // Adiciona a vírgula para representar o ponto flutuante
+    // Adiciona a vírgula para representar o ponto flutuante caso necessario
     const numeroComoFloat = parseFloat(numeroFormatado.replace(',', '.'));
 
     return numeroComoFloat;
   };
 
-  const adicionarZeros = function (valor, casasDecimais) {
+  const adicionarZeros = function(valor, casasDecimais) {
     const partes = valor.toString().split('.');
     if (partes.length === 1) {
       return valor.toFixed(casasDecimais);
     } else {
       const parteDecimal = partes[1];
-      if (parteDecimal.length < casasDecimais) {
-        return valor.toFixed(casasDecimais);
+      let resultado = '';
+
+      // Adiciona zeros à parte decimal se necessário
+      for (let i = 0; i < casasDecimais - parteDecimal.length; i++) {
+        resultado += '0';
       }
-      return valor;
+
+      return valor + resultado;
     }
   };
 
-  pesoInput.addEventListener('input', function (event) {
+  pesoInput.addEventListener('input', function(event) {
     const cursorPosition = event.target.selectionStart;
     let peso = formatarPesoAltura(event.target.value);
     peso = isNaN(peso) ? '' : peso.toString();
-    event.target.value = peso === '' ? '' : adicionarZeros(peso, 1);
+
+    // HACK: SO FUNCIONA SE COLOCAR UM PONTO
+    //formata o peso para mostrar em quilogramas
+    if (peso.length === 3) {
+      peso = peso.slice(0, 1) + ',' + peso.slice(1);
+    } else if (peso.length === 4) {
+      peso = peso.slice(0, 2) + ',' + peso.slice(2);
+    }
+
+    event.target.value = peso === '' ? '' : adicionarZeros(peso, 2); // Alterado para 2 casas decimais
     event.target.setSelectionRange(cursorPosition, cursorPosition);
+
   });
 
-  alturaInput.addEventListener('input', function (event) {
+
+
+
+
+
+  alturaInput.addEventListener('input', function(event) {
     const cursorPosition = event.target.selectionStart;
     let altura = formatarPesoAltura(event.target.value);
     altura = isNaN(altura) ? '' : altura.toString();
 
-    // Adiciona automaticamente o ponto e zeros após o primeiro número
-    if (altura.length === 1) {
-      altura += '.';
+    // Formata altura para mostrar em metros
+    if (altura.length === 3) {
+      altura = altura.slice(0, 1) + '.' + altura.slice(1);
+    } else if (altura.length === 4) {
+      altura = altura.slice(0, 2) + '.' + altura.slice(2);
     }
 
-    event.target.value = altura === '' ? '' : adicionarZeros(altura, 2);
+    event.target.value = altura;
     event.target.setSelectionRange(cursorPosition, cursorPosition);
   });
 
-  const calcularIMC = function (peso, altura) {
+  const calcularIMC = function(peso, altura) {
     if (isNaN(peso) || isNaN(altura) || altura === 0) {
       return 'Altura inválida';
     }
@@ -61,8 +82,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const imc = peso / (altura * altura);
     return imc.toFixed(2);
   };
-
-  const categorizarIMC = function (imc) {
+  const categorizarIMC = function(imc) {
     if (imc === 'Altura inválida') {
       return 'Altura inválida';
     } else if (imc <= 18.5) {
@@ -80,12 +100,12 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   };
 
-  const exibirResultado = function (imc, categoria) {
+  const exibirResultado = function(imc, categoria) {
     resultadoDiv.innerHTML = `Seu IMC é ${imc}. Categoria: ${categoria}`;
   };
 
   const form = document.getElementById('box-form');
-  form.addEventListener('submit', function (event) {
+  form.addEventListener('submit', function(event) {
     event.preventDefault();
     const peso = parseFloat(pesoInput.value.replace(',', '.'));
     const altura = parseFloat(alturaInput.value.replace(',', '.'));
